e01194246069f68b33e8072674f2f2851199129f
Unknown <dimidyuk9897@gmail.com>
2018-12-25 15:30:31 +0300

    Selenium

---
+++ Selenium/HomePage.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace Selenium
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+        private const string url = "https://www.hawaiianairlines.com";
+        By addInput = By.ClassName("ui-paxinput");
+        By addPassenger = By.ClassName("plus");
+        By addChild = By.ClassName("plus");
+        By errorsMessages = By.ClassName("field-validation-error");
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            driver.FindElement(addInput).Click();
+        }
+
+        public void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void AddPassengers()
+        {
+            driver.FindElements(addPassenger)[0].Click();
+        }
+
+        public void AddChild(int count = 1)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                driver.FindElements(addChild)[1].Click();
+            }
+        }
+
+        public IWebElement GetErrorsMessages()
+        {
+            return driver.FindElement(errorsMessages);
+        }
+    }
+}
---
+++ Selenium/HomePageTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Selenium.Tests
+{
+    [TestClass]
+    public class HomePageTests
+    {
+
+        public HomePage homePage;
+        private const string ErrorMessage = "Children under the age of 12 traveling without an accompanying adult will be subject to fees.";
+
+        [Test()]
+        public void ChildrenOnAdult()
+        {
+            OpenHomePage();
+            AddChildren();
+            AssertErrorsVisible();
+        }
+
+        private void OpenHomePage()
+        {
+            var homePage = new HomePage(new ChromeDriver());
+            homePage.OpenHomePage();
+        }
+
+        private void AddChildren()
+        {
+            homePage.AddChild(1);
+        }
+
+        private void AssertErrorsVisible()
+        {
+            var messageText = homePage.GetErrorsMessages().Text;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(ErrorMessage, messageText);
+
+        }
+    }
+}
